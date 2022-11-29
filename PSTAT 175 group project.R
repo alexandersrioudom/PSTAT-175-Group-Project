f768c9ecaa3b7ff31b9e09d54435053b97b12d86
alexandersrioudom <100656248+alexandersrioudom@users.noreply.github.com>
2022-11-28 20:50:52 -0800

    Update PSTAT 175 group project.R

--- PSTAT 175 group project.R
+++ PSTAT 175 group project.R
@@ -113,16 +113,16 @@ AIC(model1.3.2)
 #Best fitting model with considered variables of interest
 #is (model1.3)
 
-summary(model1.3.2)
+summary(model1.3)
 
 
 #CHeck proportional hazards assumption
 
-model1.3.2.coxzph <- cox.zph(model1.3.2)
+model1.3.coxzph <- cox.zph(model1.3)
 
-model1.3.2.coxzph
+model1.3.coxzph
 
-plot3 <- plot(model1.3.2.coxzph)
+plot3 <- plot(model1.3.coxzph)
 
 #It is found that the proportional hazards assumption is not 
 #violated, as the P-values are all above
